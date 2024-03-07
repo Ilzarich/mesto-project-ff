ab446eb986a3f65d4ded3950ff3c581031272570
Илья <ilatuev@MacBook-Air-Ila.local>
2024-03-07 23:42:22 +0300

    Update file

--- scripts/index.js
+++ scripts/index.js
@@ -11,16 +11,58 @@
 const cardList = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
 
-initialCards.forEach(function(cardInfo) {
-    const cardTemplateClone = cardTemplate.querySelector('.places__item').cloneNode(true);
+// initialCards.forEach(function(cardInfo) {
+//     const cardTemplateClone = cardTemplate.querySelector('.places__item').cloneNode(true);
 
+//     cardTemplateClone.querySelector('.card__image').src = cardInfo.link;
+//     cardTemplateClone.querySelector('.card__title').textContent = cardInfo.name;
+
+//     const cardDeleteBtn = cardTemplateClone.querySelector('.card__delete-button');
+//     cardDeleteBtn.addEventListener('click', () => {
+//         cardTemplateClone.remove();
+//     });
+
+//     cardList.append(cardTemplateClone);
+// });
+
+// deleteCard = card => {
+//     card.remove();
+// };
+
+// createCard = (cardInfo, deleteCardCallback) => {
+//     const cardTemplateClone = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+//     cardTemplateClone.querySelector('.card__image').src = cardInfo.link;
+//     cardTemplateClone.querySelector('.card__title').textContent = cardInfo.name;
+
+//     const cardDeleteBtn = cardTemplateClone.querySelector('.card__delete-button');
+//     cardDeleteBtn.addEventListener('click', () => {
+//         deleteCardCallback(cardTemplateClone);
+//     return cardTemplateClone;
+//     });
+// }
+
+// initialCards.forEach (cardInfo => {
+//     const newCard = createCard(cardInfo, deleteCard);
+//     cardList.append(newCard)
+// })
+
+deleteCard = card => { card.remove(); };
+
+createCard = (cardInfo, deleteCardCallback) => {
+    const cardTemplateClone = cardTemplate.querySelector('.places__item').cloneNode(true);
     cardTemplateClone.querySelector('.card__image').src = cardInfo.link;
     cardTemplateClone.querySelector('.card__title').textContent = cardInfo.name;
-
+    
     const cardDeleteBtn = cardTemplateClone.querySelector('.card__delete-button');
     cardDeleteBtn.addEventListener('click', () => {
-        cardTemplateClone.remove();
+        deleteCardCallback(cardTemplateClone);
     });
+    
+    return cardTemplateClone;
+};
 
-    cardList.append(cardTemplateClone);
+initialCards.forEach(cardInfo => {
+    const newCard = createCard(cardInfo, deleteCard);
+    cardList.append(newCard);
 });
